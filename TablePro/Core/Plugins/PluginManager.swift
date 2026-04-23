23357ea5a2ee2a482ca6734ed5d8b7b5887d7778
Ngo Quoc Dat <datlechin@gmail.com>
2026-04-23 19:21:20 +0700

    fix: bump PluginKit to v6 to reject stale plugins after DriverPlugin protocol changes (#843)

--- TablePro/Core/Plugins/PluginManager.swift
+++ TablePro/Core/Plugins/PluginManager.swift
@@ -12,7 +12,7 @@ import TableProPluginKit
 @MainActor @Observable
 final class PluginManager {
     static let shared = PluginManager()
-    static let currentPluginKitVersion = 5
+    static let currentPluginKitVersion = 6
     private static let disabledPluginsKey = "com.TablePro.disabledPlugins"
     private static let legacyDisabledPluginsKey = "disabledPlugins"
 
