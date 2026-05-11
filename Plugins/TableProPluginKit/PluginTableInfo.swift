3828ffda0e1c2ec04a022ed22c349cf53e0d8b08
Ngô Quốc Đạt <datlechin@gmail.com>
2026-05-11 09:29:36 +0700

    refactor(plugins): add schema/owner/comment metadata to PluginTableInfo (#1195)

--- Plugins/TableProPluginKit/PluginTableInfo.swift
+++ Plugins/TableProPluginKit/PluginTableInfo.swift
@@ -4,10 +4,23 @@ public struct PluginTableInfo: Codable, Sendable {
     public let name: String
     public let type: String
     public let rowCount: Int?
+    public let schema: String?
+    public let owner: String?
+    public let comment: String?
 
-    public init(name: String, type: String = "TABLE", rowCount: Int? = nil) {
+    public init(
+        name: String,
+        type: String = "TABLE",
+        rowCount: Int? = nil,
+        schema: String? = nil,
+        owner: String? = nil,
+        comment: String? = nil
+    ) {
         self.name = name
         self.type = type
         self.rowCount = rowCount
+        self.schema = schema
+        self.owner = owner
+        self.comment = comment
     }
 }
